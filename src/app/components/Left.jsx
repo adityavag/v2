ef63ab3817417bba2ae569ecbccabc5c96fd7bb8
adi247u <adi247u@gmail.com>
2023-06-30 13:43:35 +0530

    Layout Commit

---
+++ src/app/components/Left.jsx
@@ -0,0 +1,22 @@
+const Left = () => {
+    return (
+        <div>
+            <div className="mb-10  mt-24">
+                <div className="lg:grid lg:grid-cols-2 lg:gap-4 content-center">
+                    <span className="fixed">
+                        <h1 className='text-4xl sm:text-7xl font-bold text-[#C4BFB7]'>
+                            Hello !
+                        </h1>
+                        <br />
+                        <br />
+                        <h1 className='text-4xl sm:text-7xl font-bold text-[#C4BFB7]'>
+                            I'm Aditya
+                        </h1>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Left;
