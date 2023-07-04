3a431a19a181db119ff07ae053f4fcbcbe314536
adi247u <adi247u@gmail.com>
2023-07-04 21:48:05 +0530

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Left from './components/Left'
 import Right from './components/Right'
 export default function Home() {
   return (
-    <main className="lg:grid lg:grid-cols-2 lg:gap-4 max-lg:p-16 p-24 bg-[#151515] font-sans min-h-screen">
+    <main className="lg:grid lg:grid-cols-2 lg:gap-4 max-lg:p-16 p-24 md:p-6 max-sm:p-4 bg-[#151515] font-sans min-h-screen">
       <Left/>
       <Right/>
     </main>
